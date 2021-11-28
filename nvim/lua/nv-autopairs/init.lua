289522cc5a81f79138d90aeccd022e5c43fd0d1f
ZonePG <zonepg666@gmail.com>
2021-11-28 16:41:13 +0800

    update nvim/lua/nv-autopairs/init.lua nvim/lua/nv-nvimtree/init.lua

--- nvim/lua/nv-autopairs/init.lua
+++ nvim/lua/nv-autopairs/init.lua
@@ -1,3 +1,4 @@
+require("nvim-autopairs").setup {}
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
