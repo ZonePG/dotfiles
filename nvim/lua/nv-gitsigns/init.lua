7ff3620828cc7233c39949efea54d977a1008a0b
ZonePG <zonepg666@gmail.com>
2021-12-12 02:36:47 +0800

    add gitsigns

--- nvim/lua/nv-gitsigns/init.lua
+++ nvim/lua/nv-gitsigns/init.lua
@@ -1,29 +1,48 @@
-require('gitsigns').setup {
+require("gitsigns").setup {
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add = {hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn"},
+    change = {hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"},
+    delete = {hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
+    topdelete = {hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn"},
+    changedelete = {hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn"}
   },
-  numhl = false,
+  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false,
+  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   keymaps = {
     -- Default keymap options
     noremap = true,
-    buffer = true,
+    buffer = true
   },
   watch_gitdir = {
     interval = 1000,
     follow_files = true
   },
-  current_line_blame = false,
-  -- current_line_blame_delay = 1000,
-  -- current_line_blame_position = 'eol',
+  attach_to_untracked = true,
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+    delay = 300,
+    ignore_whitespace = false
+  },
+  current_line_blame_formatter_opts = {
+    relative_time = false
+  },
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  word_diff = false,
-  -- use_decoration_api = true,
-  -- use_internal_diff = true,  -- If luajit is present
+  max_file_length = 40000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = "single",
+    style = "minimal",
+    relative = "cursor",
+    row = 0,
+    col = 1
+  },
+  yadm = {
+    enable = false
+  }
 }
